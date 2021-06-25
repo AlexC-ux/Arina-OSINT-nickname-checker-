e85c13f1486d9a134b10e4501b2a589d01a83882
AlexC-ux <55258939+AlexC-ux@users.noreply.github.com>
2021-06-26 02:35:18 +0300

    Add files via upload

---
+++ Arina/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Arina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Arina checker";
+            Console.WriteLine("Select mode:\n            1 - username");
+            string mode = Console.ReadLine();
+            Console.WriteLine("\n            username:");
+            string target = Console.ReadLine();
+            switch (mode)
+                {
+                case "1":
+                    usernameMode.Start(target);
+                    break; 
+                
+                }
+                
+                
+        }
+    }
+}
---
+++ Arina/usernameMode.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace Arina
+{
+    class usernameMode
+    {
+        static public void Start(string username)
+        {
+            Console.WriteLine("Loading...");
+            
+            
+            github_username.Check(username);
+            twitter_username.Check(username);
+            tg_username.Check(username);
+            blogger_username.Check(username);
+            reddit_username.Check(username);
+
+
+            Console.WriteLine("Completed!");
+        }
+    }
+
+    class github_username
+    {
+        public static void Check(string username)
+        {
+            try
+            {
+                string url = $"https://api.github.com/users/{username}/events/public";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.UserAgent = "Mozilla / 5.0 (Windows NT 10.0; Win64; x64) AppleWebKit / 537.36";
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    Console.WriteLine("[-] GitHub : NOT FOUND");
+                }
+                else { Console.WriteLine($"[+] GitHub : https://github.com/{username}"); }
+            }
+            catch { Console.WriteLine("[-] GitHub : NOT FOUND"); }
+        }
+    }
+
+    class twitter_username
+    {
+        public static void Check(string username)
+        {
+            try
+            {
+                Console.WriteLine($"[+] Twitter : https://web.archive.org/web/2020*/twitter.com/{username}");
+            }
+            catch (Exception ex){ Console.WriteLine(ex.ToString()); }
+        }
+    }
+
+    class tg_username
+    {
+        public static void Check(string username)
+        {
+            try
+            {
+                string url = $"https://t.me/{username}";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.UserAgent = "Mozilla / 5.0 (Windows NT 10.0; Win64; x64) AppleWebKit / 537.36";
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                if (response.ResponseUri.ToString() != $"https://t.me/{username}")
+                {
+                    Console.WriteLine("[-] Telegram : NOT FOUND");
+                }
+                else { Console.WriteLine($"[+] Telegram : https://t.me/{username}"); }
+            }
+            catch { }
+            
+        }
+    }
+
+    class blogger_username
+    {
+        public static void Check(string username)
+        {
+            try
+            {
+                string url = $"http://{username}.blogspot.com";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.UserAgent = "Mozilla / 5.0 (Windows NT 10.0; Win64; x64) AppleWebKit / 537.36";
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    Console.WriteLine("[-] Blogger : NOT FOUND");
+                }
+                else { Console.WriteLine($"[+] Blogger : http://{username}.blogspot.com"); }
+            }
+            catch { }
+        }
+    }
+
+    class reddit_username
+    {
+        public static void Check(string username)
+        {
+            try
+            {
+                string url = $"https://www.reddit.com/user/{username}";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.UserAgent = "Mozilla / 5.0 (Windows NT 10.0; Win64; x64) AppleWebKit / 537.36";
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    Console.WriteLine("[-] Reddit : NOT FOUND");
+                }
+                else { Console.WriteLine($"[+] Reddit : https://www.reddit.com/user/{username}"); }
+            }
+            catch { }
+        }
+    }
+
+    class webarch_login
+    {
+        public static void Check(string username)
+        {
+            Console.WriteLine($"[+] WebArchive : https://google.com/?q=site:https://web.archive.org/web/+intext:{username}");
+        }
+    }
+
+    class vk_username
+    {
+        public static void Check(string username)
+        {
+            try
+            {
+                string url = $"https://www.reddit.com/user/{username}";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.UserAgent = "Mozilla / 5.0 (Windows NT 10.0; Win64; x64) AppleWebKit / 537.36";
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    Console.WriteLine("[-] Reddit : NOT FOUND");
+                }
+                else { Console.WriteLine($"[+] Reddit : https://www.reddit.com/user/{username}"); }
+            }
+            catch { }
+        }
+    }
+}
